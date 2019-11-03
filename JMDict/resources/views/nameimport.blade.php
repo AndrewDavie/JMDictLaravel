926fa8a4c92f95168480550ac006f41c4d9b340e
Andrew Davie <andrewdavie1973@lynx.mythic-beasts.com>
2019-11-03 19:14:57 +0000

    delete old files after rebase

--- JMDict/resources/views/nameimport.blade.php
+++
@@ -1,9 +0,0 @@
-<?php
-//ASD 211019 Trigger import of csv from frontend.
-//url /nameimport
-include "../app/ImportName.php";
-use App\Name;
-
-importnames();
-
-		?>
--- JMDict/resources/views/wordimport.blade.php
+++
@@ -1,9 +0,0 @@
-<?php
-//ASD 260919 Trigger import of csv from frontend.
-//url /wordimport
-include "app/Import.php";
-use App\Word;
-
-importwords();
-
-		?>
